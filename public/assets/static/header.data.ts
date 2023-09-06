9e183dadde537f93dfedf1566ba92cdc83599217
Gabriel-Cz <gabrielczhz@gmail.com>
2023-09-06 17:00:32 -0600

    DEV - Initial public assets

---
+++ public/assets/static/header.data.ts
@@ -0,0 +1,8 @@
+export default {
+  caption: 'Since  2019',
+  navigation: [
+    { label: 'Experience', to: '/Experience',  },
+    { label: 'About Me', to: '/',  },
+    { label: 'Contact Me', to: '/Contact',  },
+  ],
+}
